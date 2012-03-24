cc16bbc0fc24ea6aa2811f03d1878633dc5e8575
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-03-24 13:24:40 +0800

    Rename tests

--- test/LCDNumberTest.java
+++ test/LCDNumberTest.java
@@ -5,26 +5,26 @@ import static org.junit.Assert.assertThat;
 
 public class LCDNumberTest {
     @Test
-    public void should_print_single_number_0_in_digit_style() {
+    public void should_print_0() {
         LCDNumber lcdNumber = new LCDNumber("0");
         String expected =
-                " - \n" +
-                "| |\n" +
-                "   \n" +
-                "| |\n" +
-                " - \n";
+                        " - \n" +
+                        "| |\n" +
+                        "   \n" +
+                        "| |\n" +
+                        " - \n";
         assertThat(lcdNumber.toString(), is(expected));
     }
 
     @Test
-    public void should_print_single_number_1_in_digit_style() {
+    public void should_print_1() {
         LCDNumber lcdNumber = new LCDNumber("1");
         String expected =
-                "   \n" +
-                "  |\n" +
-                "   \n" +
-                "  |\n" +
-                "   \n";
+                        "   \n" +
+                        "  |\n" +
+                        "   \n" +
+                        "  |\n" +
+                        "   \n";
         assertThat(lcdNumber.toString(), is(expected));
     }
 }

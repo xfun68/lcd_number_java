891a282a3ec1c41872954231a56beb0d4218644d
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-03-24 13:24:52 +0800

    Tested 2~9

--- test/LCDNumberTest.java
+++ test/LCDNumberTest.java
@@ -27,4 +27,100 @@ public class LCDNumberTest {
                         "   \n";
         assertThat(lcdNumber.toString(), is(expected));
     }
+
+    @Test
+    public void should_print_2() {
+        LCDNumber lcdNumber = new LCDNumber("2");
+        String expected =
+                        " - \n" +
+                        "  |\n" +
+                        " - \n" +
+                        "|  \n" +
+                        " - \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_3() {
+        LCDNumber lcdNumber = new LCDNumber("3");
+        String expected =
+                        " - \n" +
+                        "  |\n" +
+                        " - \n" +
+                        "  |\n" +
+                        " - \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_4() {
+        LCDNumber lcdNumber = new LCDNumber("4");
+        String expected =
+                        "   \n" +
+                        "| |\n" +
+                        " - \n" +
+                        "  |\n" +
+                        "   \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_5() {
+        LCDNumber lcdNumber = new LCDNumber("5");
+        String expected =
+                        " - \n" +
+                        "|  \n" +
+                        " - \n" +
+                        "  |\n" +
+                        " - \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_6() {
+        LCDNumber lcdNumber = new LCDNumber("6");
+        String expected =
+                        " - \n" +
+                        "|  \n" +
+                        " - \n" +
+                        "| |\n" +
+                        " - \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_7() {
+        LCDNumber lcdNumber = new LCDNumber("7");
+        String expected =
+                        " - \n" +
+                        "  |\n" +
+                        "   \n" +
+                        "  |\n" +
+                        "   \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_8() {
+        LCDNumber lcdNumber = new LCDNumber("8");
+        String expected =
+                        " - \n" +
+                        "| |\n" +
+                        " - \n" +
+                        "| |\n" +
+                        " - \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
+
+    @Test
+    public void should_print_9() {
+        LCDNumber lcdNumber = new LCDNumber("9");
+        String expected =
+                        " - \n" +
+                        "| |\n" +
+                        " - \n" +
+                        "  |\n" +
+                        " - \n";
+        assertThat(lcdNumber.toString(), is(expected));
+    }
 }

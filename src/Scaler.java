0d95b9c2486ee8ab1453d5d1278e5ff8b71c174e
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-04-01 18:48:35 +0800

    Refactor: Removed unused constants

--- src/Scaler.java
+++ src/Scaler.java
@@ -2,10 +2,6 @@ import java.util.ArrayList;
 
 public class Scaler {
 
-    private static final int DEFAULT_SIZE = 1;
-    private static final int VERTICAL_SCALE_INTERVAL = 0;
-    private static final int HORIZONTAL_SCALE_INTERVAL = 0;
-
     private int size;
 
     public Scaler(int size) {
@@ -28,28 +24,23 @@ public class Scaler {
         StringBuffer result = new StringBuffer();
         char[] chars = line.toCharArray();
         for (int column = 0; column < chars.length; column++) {
-            result.append(duplicate_columns(column, chars[column]));
+            result.append(duplicate_columns(chars[column]));
         }
         return result.toString();
     }
 
-    private String duplicate_columns(int column, char aChar) {
+    private String duplicate_columns(char aChar) {
         StringBuffer result = new StringBuffer();
-        int scale = get_scale_for(column, HORIZONTAL_SCALE_INTERVAL);
-        for (int times = 0; times < scale; times++) result.append(aChar);
+        for (int times = 0; times < this.size; times++) result.append(aChar);
         return result.toString();
     }
 
     private String[] scale_vertically(String[] lines) {
         ArrayList<String> result = new ArrayList<String>();
         for (int line_number = 0;  line_number < lines.length; line_number++) {
-            int scale = get_scale_for(line_number, VERTICAL_SCALE_INTERVAL);
-            for (int times = 0; times < scale; times++) result.add(lines[line_number]);
+            for (int times = 0; times < this.size; times++) result.add(lines[line_number]);
         }
         return result.toArray(new String[0]);
     }
 
-    private int get_scale_for(int index, int interval) {
-        return this.size;
-    }
 }
